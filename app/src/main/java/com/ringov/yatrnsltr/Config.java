0a643b8d2645ce919ddc842ba9011d66684451ac
Ringov <by3dring@gmail.com>
2017-04-11 11:36:31 +0500

    adding base classes for api calls

---
+++ app/src/main/java/com/ringov/yatrnsltr/Config.java
@@ -0,0 +1,11 @@
+package com.ringov.yatrnsltr;
+
+/**
+ * Created by Sergey Koltsov on 11.04.2017.
+ */
+
+public class Config {
+    public static final String API_KEY = "trnsl.1.1.20170411T053738Z.2e6c9382c7b09aec.4206b04632cb03fbb33cdb90244dca5ef7083f44";
+    public static final long TIMEOUT = 20000;
+    public static final String BASE_URL = "https://translate.yandex.net/api/v1.5/tr.json/translate";
+}
---
+++ app/src/main/java/com/ringov/yatrnsltr/api/TranslationRetrofitService.java
@@ -0,0 +1,8 @@
+package com.ringov.yatrnsltr.api;
+
+/**
+ * Created by Sergey Koltsov on 11.04.2017.
+ */
+
+public interface TranslationRetrofitService {
+}
