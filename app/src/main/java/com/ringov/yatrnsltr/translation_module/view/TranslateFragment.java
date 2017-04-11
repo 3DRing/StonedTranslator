5c346510c546096fc953f35516c01b6d59bcae02
Ringov <by3dring@gmail.com>
2017-04-11 13:05:53 +0500

    adding simple textview and button for text translation showing

--- app/src/main/java/com/ringov/yatrnsltr/translation_module/view/TranslateFragment.java
+++ app/src/main/java/com/ringov/yatrnsltr/translation_module/view/TranslateFragment.java
@@ -6,17 +6,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ringov.yatrnsltr.R;
 import com.ringov.yatrnsltr.custom_views.FavoriteButton;
 import com.ringov.yatrnsltr.custom_views.ToggleImageButton;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 /**
  * Created by Sergey Koltsov on 10.04.2017.
  */
 
 public class TranslateFragment extends Fragment {
+
+    @BindView(R.id.tv_translation)
+    TextView mTvTranslation;
+
+    @OnClick(R.id.btn_translate)
+    void onTranslateClick(){
+        
+    }
 
     @Nullable
     @Override
